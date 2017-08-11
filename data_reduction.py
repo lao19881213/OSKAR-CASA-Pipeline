b20c76f78d59d915c5a1dd430652a15ce671e01a
Baoqiang Lao <lbq@shao.ac.cn>
2017-08-11 17:06:40 +0800

    add input parameter ‘Nfacets’

--- data_reduction.py
+++ data_reduction.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Simulation script.',
                                      epilog='')
     parser.add_argument('config', type=str, nargs='?', help='JSON config file.') 
+    parser.add_argument('logical_graph', type=str, nargs='?', help='logical graph file.')
     parser.add_argument('drop_uid', type=str, nargs='?', help='drop uid.')
     
     args = parser.parse_args()   
@@ -26,6 +27,15 @@ if __name__ == "__main__":
     scatter_id = scatter_uid[1] 
 
     casa.run_script(["config_file = '{}'".format(args.config)])
+    lg = json.load(open(args.logical_graph))
+    for jd in lg['nodeDataArray']:
+        if (jd['text'] == 'Scatter by Facet'):
+           for kw in ['num_of_copies', 'num_of_splits']:
+               if kw in jd:
+                  num_copy = int(jd[kw])
+              # print(num)
+
+    casa.run_script(["Nfacets = '{}'".format(num_copy)])
     casa.run_script(["NID = '{}'".format(scatter_id)])
 
     casa.run_script_from_file('/BIGDATA/ac_shao_tan_1/OSKAR/OSKAR_CASA/image.py')
